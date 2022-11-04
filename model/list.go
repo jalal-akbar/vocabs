9e995c8fd43d3a65aaf4d61a93d22def98a427f5
JalalAlbarn96 <jalal.albarn@gmail.com>
2022-11-04 19:37:52 +0700

    list

---
+++ model/list.go
@@ -0,0 +1,3 @@
+package model
+
+var ListVocab = []string{"avoid", "love", "approach", "capability", "constraint", "define", "enroll", "occasional", "require", "retrieve", "stage", "silly", "through", "tricky", "trace", "willing"}
