1670b4eafd5aebf270a3bdf83113a21396babc60
JalalAlbarn96 <jalal.albarn@gmail.com>
2022-11-04 19:38:09 +0700

    translates

---
+++ model/transnlates.go
@@ -0,0 +1,20 @@
+package model
+
+var VocabTranslate = map[string]string{
+	"avoid":      "Menghindari, Mencegah",
+	"love":       "Cinta",
+	"approach":   "Mendekati",
+	"capability": "kemampuan",
+	"constraint": "kendala",
+	"define":     "menetapkan",
+	"enroll":     "mendaftar",
+	"occasional": "sesekalli",
+	"require":    "memerlukan",
+	"retrieve":   "mengambil",
+	"stage":      "tahap",
+	"silly":      "konyol",
+	"through":    "melalui",
+	"tricky":     "sulit",
+	"trace":      "jejak",
+	"willing":    "bersedia",
+}
