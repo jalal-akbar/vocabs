4d9318966adb7b3f19d35779a00b7571bf991925
JalalAlbarn96 <jalal.albarn@gmail.com>
2022-11-04 19:54:28 +0700

    looping

--- main.go
+++ main.go
@@ -4,17 +4,11 @@ import (
 	"github.com/jalal-akbar/vocabs/model"
 )
 
-// var vocabs = []string{"avoid", "love"}
-
 func main() {
-
-	//vocabs := []string{"Avoid", "Love"}
-	//getVocabs := model.GetVocab(vocabs)
-
 	for matching := false; !matching; {
-		guess := model.GetInputVocabs()
-
-		matching = model.IsMatching(model.ListVocab, guess)
+		for i := 0; i < 100; i++ {
+			guess := model.GetInputVocabs()
+			matching = model.IsMatching(model.ListVocab, guess)
+		}
 	}
-
 }
